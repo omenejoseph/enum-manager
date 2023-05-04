7dd7de9c12127ac22cf021613c432c3202755f50
Omene Joseph <31065489+omenejoseph@users.noreply.github.com>
2023-05-04 16:24:24 +0100

    Update tests/EnumManagerTest.php in PR #3

--- tests/EnumManagerTest.php
+++ tests/EnumManagerTest.php
@@ -1,53 +1,43 @@
 
+```php
 <?php
 
-namespace OmeneJoseph\EnumManager\Test;
+namespace OmeneJoseph\EnumManager\Tests;
 
 use OmeneJoseph\EnumManager\EnumManager;
-use OmeneJoseph\EnumManager\Contracts\EnumManagerInterface;
-use OmeneJoseph\EnumManager\Test\Fixtures\TestEnum;
-use PHPUnit\Framework\TestCase;
 
-class EnumManagerTest extends TestCase
+class ExampleEnum
 {
-    private EnumManagerInterface $enumManager;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->enumManager = new EnumManager();
-    }
-
-    /** @test */
-    public function it_returns_enum_values_as_a_string()
-    {
-        $result = $this->enumManager->enumValues(TestEnum::class);
-
-        $this->assertSame('John,Doe,25', $result);
-    }
-
-    /** @test */
-    public function it_returns_enum_keys_as_a_string()
-    {
-        $result = $this->enumManager->enumKeys(TestEnum::class);
-
-        $this->assertSame('FIRST_NAME,LAST_NAME,AGE', $result);
-    }
-
-    /** @test */
-    public function it_returns_enum_values_as_an_array()
-    {
-        $result = $this->enumManager->enumValuesArray(TestEnum::class);
-
-        $this->assertSame(['John', 'Doe', 25], $result);
-    }
-
-    /** @test */
-    public function it_returns_enum_keys_as_an_array()
-    {
-        $result = $this->enumManager->enumKeysArray(TestEnum::class);
-
-        $this->assertSame(['FIRST_NAME', 'LAST_NAME', 'AGE'], $result);
-    }
+    const ONE = 'one';
+    const TWO = 'two';
+    const THREE = 'three';
 }
 
+it('returns enum values as a string', function () {
+    $manager = new EnumManager();
+    $values = $manager->enumValues(ExampleEnum::class);
+
+    expect($values)->toEqual('one,two,three');
+});
+
+it('returns enum keys as a string', function () {
+    $manager = new EnumManager();
+    $keys = $manager->enumKeys(ExampleEnum::class);
+
+    expect($keys)->toEqual('ONE,TWO,THREE');
+});
+
+it('returns enum values as an array', function () {
+    $manager = new EnumManager();
+    $values = $manager->enumValuesArray(ExampleEnum::class);
+
+    expect($values)->toEqual(['one', 'two', 'three']);
+});
+
+it('returns enum keys as an array', function () {
+    $manager = new EnumManager();
+    $keys = $manager->enumKeysArray(ExampleEnum::class);
+
+    expect($keys)->toEqual(['ONE', 'TWO', 'THREE']);
+});
+```
